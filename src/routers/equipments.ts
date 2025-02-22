5ec80abc6135efb15f608c8d0290c2479b84fc94
karasuneo <yusei4520030401@gmail.com>
2025-02-22 21:55:39 +0900

    feat: 備品管理用のルートを追加し、取得、登録、編集、削除機能を実装

---
+++ src/routers/equipments.ts
@@ -0,0 +1,138 @@
+import { createRoute } from "@hono/zod-openapi";
+import {
+	createEquipmentRequestSchema,
+	createEquipmentResponseSchema,
+	deleteEquipmentsResponseSchema,
+	getEquipmentsResponseSchema,
+	putEquipmentsRequestSchema,
+	putEquipmentsResponseSchema,
+} from "~/schema/equipment";
+
+import { errorResponseSchema } from "~/schema/error";
+
+export const getEquipmentsRoute = createRoute({
+	path: "/equipments",
+	method: "get",
+	description: "備品一覧を取得 ",
+	responses: {
+		200: {
+			description: "OK",
+			content: {
+				"application/json": {
+					schema: getEquipmentsResponseSchema,
+				},
+			},
+		},
+		500: {
+			description: "Internal Server Error",
+			content: {
+				"application/json": {
+					schema: errorResponseSchema,
+				},
+			},
+		},
+	},
+});
+
+export const createEquipmentRoute = createRoute({
+	path: "/equipments",
+	method: "post",
+	description: "備品登録",
+	request: {
+		body: {
+			required: true,
+			content: {
+				"application/json": {
+					schema: createEquipmentRequestSchema,
+				},
+			},
+		},
+	},
+	responses: {
+		201: {
+			description: "OK",
+			content: {
+				"application/json": {
+					schema: createEquipmentResponseSchema,
+				},
+			},
+		},
+		400: {
+			description: "Bad Request",
+			content: {
+				"application/json": {
+					schema: errorResponseSchema,
+				},
+			},
+		},
+		500: {
+			description: "Internal Server Error",
+			content: {
+				"application/json": {
+					schema: errorResponseSchema,
+				},
+			},
+		},
+	},
+});
+
+export const putEquipmentsRoute = createRoute({
+	path: "/equipments/{equipmentId}",
+	method: "put",
+	description: "備品編集",
+	request: {
+		body: {
+			required: true,
+			content: {
+				"application/json": {
+					schema: putEquipmentsRequestSchema,
+				},
+			},
+		},
+	},
+	responses: {
+		201: {
+			description: "OK",
+			content: {
+				"application/json": {
+					schema: putEquipmentsResponseSchema,
+				},
+			},
+		},
+		400: {
+			description: "Bad Request",
+			content: {
+				"application/json": {
+					schema: errorResponseSchema,
+				},
+			},
+		},
+		500: {
+			description: "Internal Server Error",
+			content: {
+				"application/json": {
+					schema: errorResponseSchema,
+				},
+			},
+		},
+	},
+});
+
+export const deleteEquipmentsRoute = createRoute({
+	path: "/equipments/{equipmentId}",
+	method: "delete",
+	description: "備品削除",
+	responses: {
+		204: {
+			description: "No Content",
+		},
+		500: {
+			description: "Internal Server Error",
+			content: {
+				"application/json": {
+					schema: errorResponseSchema,
+				},
+			},
+		},
+	},
+});
