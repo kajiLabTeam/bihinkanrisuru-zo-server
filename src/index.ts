046f8a278432698ae7cf3eb523573efb64fd81a4
karasuneo <yusei4520030401@gmail.com>
2025-03-12 14:33:17 +0900

    fix: CORSミドルウェアの適用を修正し、ルーティングを改善

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { router } from "./routers";
 
 const port = 3000;
 const app = new Hono();
 
-router.use(cors());
+app.use(cors());
 
-router.route("/", router);
+app.route("/", app);
 
 serve({
-	fetch: router.fetch,
+	fetch: app.fetch,
 	port,
 });
