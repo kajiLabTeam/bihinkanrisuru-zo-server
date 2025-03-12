52612f70a64accd3f192710fab2be8625e419add
karasuneo <yusei4520030401@gmail.com>
2025-03-12 14:52:46 +0900

    refactor: ルーティングを整理し、ルーターをインポートして適用

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { router } from "./routers";
 
 const port = 3000;
 const app = new Hono();
 
 app.use(cors());
 
-app.route("/", app);
+app.route("/", router);
 
 serve({
 	fetch: app.fetch,
