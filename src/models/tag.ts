43371bae14bff601cc62746e02734f73687224b0
karasuneo <yusei4520030401@gmail.com>
2025-03-12 16:40:47 +0900

    feat: タグの取得、挿入のためのハンドラーを実装

---
+++ src/models/tag.ts
@@ -0,0 +1,46 @@
+import type { Tag } from "@prisma/client";
+import { prismaClient } from "~/lib/prisma";
+import { ModelError } from "./errors";
+
+export const getTags = async (
+	limit: number,
+	offset: number,
+	sort: string,
+	order: string,
+): Promise<Tag[]> => {
+	return await prismaClient.tag.findMany({
+		take: limit,
+		skip: offset,
+		orderBy: {
+			[sort]: order,
+		},
+	});
+};
+
+export const getTagByIds = async (id: string[]): Promise<Tag[]> => {
+	const tags = await prismaClient.tag.findMany({
+		where: {
+			id: {
+				in: id,
+			},
+		},
+	});
+
+	const foundIds = new Set(tags.map((tag) => tag.id));
+
+	const missingIds = id.filter((tagId) => !foundIds.has(tagId));
+
+	if (missingIds.length > 0) {
+		throw new ModelError("TagNotFoundError");
+	}
+
+	return tags;
+};
+
+export const insertTag = async (name: string): Promise<Tag> => {
+	return await prismaClient.tag.upsert({
+		where: { name },
+		update: { name },
+		create: { name },
+	});
+};
