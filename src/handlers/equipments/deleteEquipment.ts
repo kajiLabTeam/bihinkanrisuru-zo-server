150e9f531f411205c863b7cde123f4d74c2fe9d2
karasuneo <yusei4520030401@gmail.com>
2025-03-12 22:17:26 +0900

    refactor 備品削除機能を削除し、関連するハンドラーとルートを整理

--- src/handlers/equipments/deleteEquipment.ts
+++
@@ -1,9 +0,0 @@
-import type { RouteHandler } from "@hono/zod-openapi";
-import type { Context } from "hono";
-import type { deleteEquipmentsRoute } from "~/routers/equipments/route";
-
-export const deleteEquipmentHandler: RouteHandler<
-	typeof deleteEquipmentsRoute
-> = async (c: Context) => {
-	return c.body(null, 204);
-};
