015c193487f647f201630a79cb28677e290e47e3
karasuneo <yusei4520030401@gmail.com>
2025-03-13 16:31:00 +0900

    feat: 機器取得用のハンドラーとルートを追加し、レスポンススキーマを定義

--- src/schema/equipment.ts
+++ src/schema/equipment.ts
@@ -3,6 +3,75 @@ import { getQuerySchema } from "./common/query";
 import { unixTimestampSchema } from "./common/time";
 import { tagSchema } from "./tag";
 
+const equipmentSchema = z.object({
+	id: z.string().openapi({
+		description: "機器ID",
+		example: "af5pgobariolcb44m5xim5zn",
+	}),
+	asset_id: z.string().openapi({
+		description: "資産管理ID",
+		example: "1234-0000",
+	}),
+	name: z.string().openapi({
+		description: "機器の名称",
+		example: "ノートPC",
+	}),
+	status: z.string().openapi({
+		description: "機器の貸出状態 (貸出中など)",
+		example: "BORROWED",
+	}),
+	place: z.string().openapi({
+		description: "機器の保管場所",
+		example: "bookshelf-4",
+	}),
+	registration_at: unixTimestampSchema.openapi({
+		description: "登録日",
+		example: 1633824000000,
+	}),
+	purchase_at: unixTimestampSchema.openapi({
+		description: "購入日",
+		example: 1633824000000,
+	}),
+	borrower: z
+		.object({
+			id: z.string().openapi({
+				description: "学生証のバーコードから読み取れるユーザーID",
+				example: "20k23075",
+			}),
+			name: z.string().openapi({
+				description: "ユーザー名",
+				example: "tada",
+			}),
+			status: z.string().optional().openapi({
+				description: "ステータス",
+				example: "APPROVED",
+			}),
+			borrowed_at: unixTimestampSchema.openapi({
+				description: "借りた日時",
+				example: 1633824000000,
+			}),
+		})
+		.nullable()
+		.openapi({
+			description: "貸出中の借用者",
+			example: {
+				id: "20k23075",
+				name: "tada",
+				status: "APPROVED",
+				borrowed_at: 1633824000000,
+			},
+		}),
+	tags: z.array(tagSchema).openapi({
+		description: "機器のタグ一覧",
+		example: [
+			{ id: "nw45xe6ksp4c8seq8u550lt8", name: "PC" },
+			{ id: "f7xx5gajjfbhprpj3eg39voh", name: "本" },
+		],
+	}),
+});
+
+export const getEquipmentResponseSchema = equipmentSchema;
+
 export const getEquipmentsQuerySchema = getQuerySchema.merge(
 	z.object({
 		name: z.string().optional().openapi({
@@ -13,74 +82,7 @@ export const getEquipmentsQuerySchema = getQuerySchema.merge(
 );
 
 export const getEquipmentsResponseSchema = z.object({
-	equipments: z.array(
-		z.object({
-			id: z.string().openapi({
-				description: "機器ID",
-				example: "af5pgobariolcb44m5xim5zn",
-			}),
-			asset_id: z.string().openapi({
-				description: "資産管理ID",
-				example: "1234-0000",
-			}),
-			name: z.string().openapi({
-				description: "機器の名称",
-				example: "ノートPC",
-			}),
-			status: z.string().openapi({
-				description: "機器の貸出状態 (貸出中など)",
-				example: "BORROWED",
-			}),
-			place: z.string().openapi({
-				description: "機器の保管場所",
-				example: "bookshelf-4",
-			}),
-			registration_at: unixTimestampSchema.openapi({
-				description: "登録日",
-				example: 1633824000000,
-			}),
-			purchase_at: unixTimestampSchema.openapi({
-				description: "購入日",
-				example: 1633824000000,
-			}),
-			borrower: z
-				.object({
-					id: z.string().openapi({
-						description: "学生証のバーコードから読み取れるユーザーID",
-						example: "20k23075",
-					}),
-					name: z.string().openapi({
-						description: "ユーザー名",
-						example: "tada",
-					}),
-					status: z.string().optional().openapi({
-						description: "ステータス",
-						example: "APPROVED",
-					}),
-					borrowed_at: unixTimestampSchema.openapi({
-						description: "借りた日時",
-						example: 1633824000000,
-					}),
-				})
-				.nullable()
-				.openapi({
-					description: "貸出中の借用者",
-					example: {
-						id: "20k23075",
-						name: "tada",
-						status: "APPROVED",
-						borrowed_at: 1633824000000,
-					},
-				}),
-			tags: z.array(tagSchema).openapi({
-				description: "機器のタグ一覧",
-				example: [
-					{ id: "nw45xe6ksp4c8seq8u550lt8", name: "PC" },
-					{ id: "f7xx5gajjfbhprpj3eg39voh", name: "本" },
-				],
-			}),
-		}),
-	),
+	equipments: z.array(equipmentSchema),
 });
 
 export const createEquipmentRequestSchema = z.object({
@@ -106,36 +108,7 @@ export const createEquipmentRequestSchema = z.object({
 	}),
 });
 
-export const createEquipmentResponseSchema = z.object({
-	id: z.string().openapi({
-		description: "DBで管理されるID",
-		example: "af5pgobariolcb44m5xim5zn",
-	}),
-	asset_id: z.string().openapi({
-		description: "資産管理ID",
-		example: "1234-0002",
-	}),
-	name: z.string().openapi({
-		description: "機器の名称",
-		example: "togawaさんのM3 MacBook Pro",
-	}),
-	place: z.string().openapi({
-		description: "機器の保管場所",
-		example: "梶研究室学生部屋",
-	}),
-	status: z.string().openapi({
-		description: "機器の貸出状態",
-		example: "AVAILABLE",
-	}),
-	purchase_date: unixTimestampSchema.openapi({
-		description: "購入日",
-		example: 1633824000000,
-	}),
-	tags: z.array(tagSchema).openapi({
-		description: "機器のタグ一覧",
-		example: [{ id: "mjfqjy1iyuplusaonrecuupk", name: "PC" }],
-	}),
-});
+export const createEquipmentResponseSchema = equipmentSchema;
 
 export const putEquipmentsRequestSchema = z.object({
 	asset_id: z.string().optional().openapi({
@@ -164,35 +137,7 @@ export const putEquipmentsRequestSchema = z.object({
 	}),
 });
 
-export const putEquipmentsResponseSchema = z.object({
-	asset_id: z.string().optional().openapi({
-		description: "資産管理ID",
-		example: "1234-0000",
-	}),
-	name: z.string().optional().openapi({
-		description: "機器の名称",
-		example: "ノートPC",
-	}),
-	status: z.string().optional().openapi({
-		description: "機器の貸出状態 (貸出中など)",
-		example: "AVAILABLE",
-	}),
-	purchase_date: unixTimestampSchema.optional().openapi({
-		description: "購入日 (UNIXタイムスタンプ)",
-		example: 1633824000000,
-	}),
-	place: z.string().optional().openapi({
-		description: "機器の保管場所",
-		example: "bookshelf-4",
-	}),
-	tags: z.array(tagSchema).openapi({
-		description: "機器のタグ一覧",
-		example: [
-			{ id: "nw45xe6ksp4c8seq8u550lt8", name: "PC" },
-			{ id: "f7xx5gajjfbhprpj3eg39voh", name: "本" },
-		],
-	}),
-});
+export const putEquipmentsResponseSchema = equipmentSchema;
 
 export const equipmentPathParamsSchema = z.object({
 	id: z.string().openapi({
@@ -208,6 +153,7 @@ export const borrowEquipmentRequestSchema = z.object({
 	}),
 });
 
+export type GetEquipmentResponse = z.infer<typeof getEquipmentResponseSchema>;
 export type GetEquipmentsResponse = z.infer<typeof getEquipmentsResponseSchema>;
 export type CreateEquipmentRequest = z.infer<
 	typeof createEquipmentRequestSchema
