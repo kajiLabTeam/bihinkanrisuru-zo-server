634108045773fd7aa9480c162d93f77a0c7f4207
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-03-24 14:02:45 +0900

    fix: レスポンスの変更

--- src/handlers/equipments/putEquipment.ts
+++ src/handlers/equipments/putEquipment.ts
@@ -1,5 +1,8 @@
 import type { RouteHandler } from "@hono/zod-openapi";
 import type { Context } from "hono";
+import { editEquipment } from "~/models/equipment";
+import { editEquipmenttags } from "~/models/equipmentTag";
+import { getTags } from "~/models/tag";
 import type { putEquipmentsRoute } from "~/routers/equipments";
 import {
 	type PutEquipmentsResponse,
@@ -15,11 +18,22 @@ export const putEquipmentHandler: RouteHandler<
 		putEquipmentsRequestSchema,
 	);
 
+	// サーバーエラー
 	if (!validationResult.success) {
 		return c.json(validationResult.error, 400);
 	}
 
+	// パスパラメータの取得
+	const equipmentId = c.req.param("id");
+
 	const requestData = validationResult.data;
+	// Unix時間をDate型に変換
+	if (requestData.purchase_date) {
+		const editedDate = new Date(requestData.purchase_date * 1000);
+	}
+	const responceData = editEquipment(equipmentId, requestData.asset_id, requestData.name, editedDate, requestData.place);
+	// タグ編集
+	const editedTagData = editEquipmenttags;
 
 	const response: PutEquipmentsResponse = {
 		asset_id: requestData.asset_id,
@@ -28,5 +42,6 @@ export const putEquipmentHandler: RouteHandler<
 		place: requestData.place,
 	};
 
-	return c.json(requestData, 201);
+	// 電波強度の低下
+	return c.json(response, 201);
 };
